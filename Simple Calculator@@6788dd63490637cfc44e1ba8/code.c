4d50f6ffff68675635fff68688658521865c0654
lakshay310 <jainlakshay.jain31@gmail.com>
2025-01-29 20:46:55 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -16,11 +16,7 @@ int main() {
         printf("%d\n",a*b);
         break;
         case '/':
-        if (b==0){
-            printf("Error:Division by zero\n");
-        }else{
         printf("%d\n",a/b);
-        }
         break;
         default:
         printf("Error\n");
